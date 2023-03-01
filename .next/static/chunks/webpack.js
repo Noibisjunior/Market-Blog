9989c1fda5514ac110189685eb38b8737adeed92
Noibisjunior <noibisjunior22@gmail.com>
2023-03-01 21:34:30 +0100

    Market-Blog built with NextJS

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ec0b144cc9180de8"; }
+/******/ 		__webpack_require__.h = function() { return "3d7823f0a3899afa"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import Header from "../components/header";
 import { Button } from 'react-bootstrap';
 
 
+
+
 const Component = () => {
   return (
     <div className="relative w-full flex flex-row p-2.5 box-border items-start justify-start text-left text-xs text-gray-200 font-poppins">
